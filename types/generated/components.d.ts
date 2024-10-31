7ab7e27e7f25572ff8de8f40513373e9bd98c9ef
yerbaMatte <milosz.lewandowski@icloud.com>
2024-10-31 11:18:24 +0100

    strapi

---
+++ types/generated/components.d.ts
@@ -0,0 +1,44 @@
+import type { Struct, Schema } from '@strapi/strapi';
+
+export interface FeaturesRicharea extends Struct.ComponentSchema {
+  collectionName: 'components_features_richareas';
+  info: {
+    displayName: 'richarea';
+    icon: 'apps';
+  };
+  attributes: {
+    content: Schema.Attribute.Blocks;
+  };
+}
+
+export interface FeaturesImage extends Struct.ComponentSchema {
+  collectionName: 'components_features_images';
+  info: {
+    displayName: 'image';
+    icon: 'picture';
+  };
+  attributes: {
+    image: Schema.Attribute.Media<'images' | 'files'>;
+  };
+}
+
+export interface FeaturesCode extends Struct.ComponentSchema {
+  collectionName: 'components_features_codes';
+  info: {
+    displayName: 'code';
+    icon: 'code';
+  };
+  attributes: {
+    code: Schema.Attribute.RichText;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'features.richarea': FeaturesRicharea;
+      'features.image': FeaturesImage;
+      'features.code': FeaturesCode;
+    }
+  }
+}
